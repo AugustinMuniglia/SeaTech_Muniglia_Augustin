a0532d81fa197174d438d4cc9686c5a7b9c53ec1
Robot <touloniut@gmail.com>
2022-09-26 17:33:08 +0200

    26 sept

---
+++ Embedded/Robot_Muniglia_Lieutier.X/timer.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   timer.h
+ * Author: TP-EO-1
+ *
+ * Created on 26 septembre 2022, 16:28
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+void InitTimer23(void);
+void InitTimer1(void);
+
+#endif	/* TIMER_H */
+
